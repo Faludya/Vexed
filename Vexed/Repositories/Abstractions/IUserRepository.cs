d6dcf4c7db13f5cbead08ccb7d3b580eafc38ac5
Frasineanu Claudia <60782303+Faludya@users.noreply.github.com>
2023-03-20 21:45:15 +0200

    UserName dropdown

--- Vexed/Repositories/Abstractions/IUserRepository.cs
+++ Vexed/Repositories/Abstractions/IUserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Vexed.Models;
+using Vexed.Models.ViewModels;
 
 namespace Vexed.Repositories.Abstractions
 {
@@ -9,5 +10,15 @@ namespace Vexed.Repositories.Abstractions
         /// Returns the Guid of the superior for the given user.
         /// </summary>
         Guid GetUserSuperior(Guid userId);
+
+        /// <summary>
+        /// Returns all the identity users
+        /// </summary>
+        List<IdentityUser> GetAllUsers();
+
+        /// <summary>
+        /// Returns all the UserIds and UserNames of the users that are 
+        /// </summary>
+        List<UserNameVM> GetUnassignedUserNames();
     }
 }
