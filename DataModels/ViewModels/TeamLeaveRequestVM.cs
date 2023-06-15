18ca2a846273c085321ce3bde6a847599af849a6
Frasineanu Claudia <60782303+Faludya@users.noreply.github.com>
2023-06-15 13:27:44 +0300

    added team calendar

---
+++ DataModels/ViewModels/TeamLeaveRequestVM.cs
@@ -0,0 +1,8 @@
+﻿namespace DataModels.ViewModels
+{
+    public class TeamLeaveRequestVM
+    {
+        public string UserName { get; set; }
+        public List<LeaveRequest> Leaves { get; set; }
+    }
+}
